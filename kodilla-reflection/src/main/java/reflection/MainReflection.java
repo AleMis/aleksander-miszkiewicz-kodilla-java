493dd3bf678e6ba16b6deef1e81784e447c7bdfb
AleMis <aleksander.miszkiewicz@gmail.com>
2017-10-02 23:41:38 +0200

    Java Reflection - Exercise 1 - changes

--- kodilla-reflection/src/main/java/reflection/MainReflection.java
+++ kodilla-reflection/src/main/java/reflection/MainReflection.java
@@ -7,10 +7,16 @@ public class MainReflection {
     public static void main(String args[]) {
 
         Class myClass = MyObject.class;
-        Field[] myClassFields = myClass.getFields();
-        for(int i = 0; i<myClassFields.length; i++) {
-            System.out.println(myClassFields[i]);
-            System.out.println(myClassFields[i].getType());
+        reflection(myClass);
+
+    }
+
+
+    public static void reflection(Class myclass) {
+        Field[] fields = myclass.getFields();
+        for(int i = 0; i<fields.length; i++) {
+            System.out.println("Field = " + fields[i]);
+            System.out.println("Field type = " + fields[i].getType());
         }
     }
 }
